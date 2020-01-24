4689d42fa8b5e80db874f72fe831c2301bc7875b
WillMandil001 <Will.Mandil@stelram.co.uk>
2020-01-24 22:36:14 +0000

    updated code structure for more readable code - convert_to_topo_nav.py

--- thorvald_explorer/src/machine_vision/convert_to_topo_nav.py
+++ thorvald_explorer/src/machine_vision/convert_to_topo_nav.py
@@ -24,29 +24,35 @@ class convert_to_topo_nav():
         self.camera_model = image_geometry.PinholeCameraModel()
         self.camera_model.fromCameraInfo(self.camera_info)
 
-    def calculate_waypoints(self):  # ran once to segment the weeds and locate them
+    def import_image(self):
         image = rospy.wait_for_message("/thorvald_002/kinect2_camera/hd/image_color_rect", Image)
-        # self.last_ts = image.header.stamp
         cv_image = self.bridge.imgmsg_to_cv2(image, "bgr8")
-        cv_image = cv2.resize(cv_image, None, fx=0.5, fy=0.5)  # Resize
+        return cv_image        
 
+    def remove_soil(self, cv_image):  # ran once to segment the weeds and locate them
         hsv_img = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)  # convert to hsv for better color segmentation:
-        cv2.imshow("hsv_img", hsv_img[:,:,0])
         lower_green = np.array([20,0,0])
         upper_green = np.array([255,255,255])
         mask = cv2.inRange(hsv_img, lower_green, upper_green)
-        cv2.imshow("mask", mask)
 
-        kernel = np.ones((2,2),np.uint8)
+        kernel = np.ones((3,3),np.uint8)
         opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-        cv2.imshow("opening", opening)
+        return opening
 
+    def calculate_crop_rows(self, cv_image):
+        cv_image = cv2.resize(cv_image, None, fx=0.5, fy=0.5)  # Resize
+        cv2.imshow("cv_image", cv_image)
         k = cv2.waitKey(0)
         if k ==27:
             pass
         cv2.destroyAllWindows()
 
+
 if __name__ == '__main__':
     rospy.init_node('convert_to_topo_nav', anonymous=True)
     convert = convert_to_topo_nav()
-    convert.calculate_waypoints()
+    raw_image = convert.import_image()
+    no_soil_image = convert.remove_soil(raw_image)
+    convert.calculate_crop_rows(no_soil_image)
+
+
