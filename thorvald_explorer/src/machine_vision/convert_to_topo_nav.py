50b7e795f39e512413f05cd4817301bb4016165c
WillMandil001 <Will.Mandil@stelram.co.uk>
2020-01-25 19:21:17 +0000

    roughly working hough line segmentation for each crop row.

--- thorvald_explorer/src/machine_vision/convert_to_topo_nav.py
+++ thorvald_explorer/src/machine_vision/convert_to_topo_nav.py
@@ -27,7 +27,7 @@ class convert_to_topo_nav():
     def import_image(self):
         image = rospy.wait_for_message("/thorvald_002/kinect2_camera/hd/image_color_rect", Image)
         cv_image = self.bridge.imgmsg_to_cv2(image, "bgr8")
-        return cv_image        
+        return cv_image
 
     def remove_soil(self, cv_image):  # ran once to segment the weeds and locate them
         hsv_img = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)  # convert to hsv for better color segmentation:
@@ -40,7 +40,41 @@ class convert_to_topo_nav():
         return opening
 
     def calculate_crop_rows(self, cv_image):
-        cv_image = cv2.resize(cv_image, None, fx=0.5, fy=0.5)  # Resize
+        # cv_image = cv2.resize(cv_image, None, fx=0.2, fy=0.2)  # Resize
+        cv2.imshow("cv_image", cv_image)
+
+        kernel = np.ones((10,10),np.uint8)
+        dilation = cv2.dilate(cv_image,kernel,iterations = 1)
+        cv2.imshow("dilationdilation", dilation)
+
+        minLineLength = 100
+        maxLineGap = 100
+        lines = cv2.HoughLinesP(dilation,1,np.pi/180,100,minLineLength,maxLineGap)
+        for i in range(0, 10):
+            for x1,y1,x2,y2 in lines[i]:
+                cv2.line(dilation,(x1,y1),(x2,y2),(100,100,10),2)
+
+
+        # cv2.HoughLines(image, rho, theta, threshold[, lines[, srn[, stn]]])
+        # edges = cv2.Canny(gray,50,150,apertureSize = 3)
+        for i in range(0, 100):
+            try:
+                lines = cv2.HoughLines(dilation,1,np.pi/90,300)
+                for rho,theta in lines[0]:
+                    a = np.cos(theta)
+                    b = np.sin(theta)
+                    x0 = a*rho
+                    y0 = b*rho
+                    x1 = int(x0 + 10000*(-b))
+                    y1 = int(y0 + 10000*(a))
+                    x2 = int(x0 - 10000*(-b))
+                    y2 = int(y0 - 10000*(a))
+                    cv2.line(cv_image,(x1,y1),(x2,y2),(50,50,100), 5)
+                    cv2.line(dilation,(x1,y1),(x2,y2),(0,0,255), 50)
+            except:
+                break
+
+        cv2.imshow("dilation", dilation)
         cv2.imshow("cv_image", cv_image)
         k = cv2.waitKey(0)
         if k ==27:
