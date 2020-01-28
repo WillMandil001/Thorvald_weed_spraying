ed48ec50ba2f806d9bc13b8bb9d6cdea38aa35b5
WillMandil001 <Will.Mandil@stelram.co.uk>
2020-01-28 00:19:24 +0000

    working histograms - i think - not fully tested yet

--- thorvald_explorer/src/machine_vision/histogram_solution.py
+++ thorvald_explorer/src/machine_vision/histogram_solution.py
@@ -6,6 +6,8 @@ import tf2_ros
 import rospy
 import numpy as np
 import image_geometry
+from matplotlib import pyplot as plt
+
 from std_msgs.msg import String
 from sensor_msgs.msg import Image, CameraInfo
 from geometry_msgs.msg import PoseStamped, PoseArray
@@ -56,28 +58,101 @@ class convert_to_topo_nav():
         prev_point_h = 0
         for i in range(0, imgx_l, box_size):
             for j in range(0, imgy_h, box_size):
-                print("i = ", i, "j = ", j)
-                image_square = cv_image[i:(i+box_size), j:(j+box_size)]      
+                # print("i = ", i, "j = ", j)
+                image_square = cv_image[i:(i+box_size), j:(j+box_size)]
                 # 1. Find center of image.
                 imgx = (image_square.shape[1] / 2)
                 imgy = (image_square.shape[0] / 2)
 
                 # 2. Calculate lines for each angle (0, 179) degrees and do for 
-                for angle in range(0, 180):
+                histogram = []
+                angles = []
+                for angle in range(0, 180, 10):
+                    histo = 0
                     angle_rad = (angle * math.pi) / 180
                     r = 100  # R = LENGTH OF LINE DRAWN. JUST NEEDS TO BE LONGER THAN THE EDGES OF THE image_square
                     for a in range(-r, r + 1, max(1, int((r * 2)))):
                         xcirc = imgx + a * math.cos(angle_rad)
                         ycirc = imgy + a * math.sin(angle_rad)
-                        cv2.line(image_square, (int(imgx), int(imgy)), (int(xcirc), int(ycirc)), (100, 100, 100), thickness=1, lineType=8)
+                        points = self.createLineIterator((int(imgx), int(imgy)), (int(xcirc), int(ycirc)), image_square)
+                        histo += sum(points[:, 2])
+                    histogram.append(histo)
+                    angles.append(angle)
+
+                plt.bar(angles, histogram, align='center', alpha=0.5)
+                plt.xticks(angles, histogram)
+                plt.ylabel('Usage')
+                plt.title('Programming language usage')
+                plt.show()
 
                 cv2.imshow("image_square", image_square)
                 k = cv2.waitKey(0)
                 if k ==27:
                     break
                 cv2.destroyAllWindows()
-                    # 3. buld histogram from sum of white pixels in line.
+                # 3. buld histogram from sum of white pixels in line.
 
+
+    def createLineIterator(self, P1, P2, image_square):
+       imageH = image_square.shape[0]
+       imageW = image_square.shape[1]
+       P1X = P1[0]
+       P1Y = P1[1]
+       P2X = P2[0]
+       P2Y = P2[1]
+
+       #difference and absolute difference between points
+       #used to calculate slope and relative location between points
+       dX = P2X - P1X
+       dY = P2Y - P1Y
+       dXa = np.abs(dX)
+       dYa = np.abs(dY)
+
+       #predefine numpy array for output based on distance between points
+       itbuffer = np.empty(shape=(np.maximum(dYa,dXa),3),dtype=np.float32)
+       itbuffer.fill(np.nan)
+
+       #Obtain coordinates along the line using a form of Bresenham's algorithm
+       negY = P1Y > P2Y
+       negX = P1X > P2X
+       if P1X == P2X: #vertical line segment
+           itbuffer[:,0] = P1X
+           if negY:
+               itbuffer[:,1] = np.arange(P1Y - 1,P1Y - dYa - 1,-1)
+           else:
+               itbuffer[:,1] = np.arange(P1Y+1,P1Y+dYa+1)              
+       elif P1Y == P2Y: #horizontal line segment
+           itbuffer[:,1] = P1Y
+           if negX:
+               itbuffer[:,0] = np.arange(P1X-1,P1X-dXa-1,-1)
+           else:
+               itbuffer[:,0] = np.arange(P1X+1,P1X+dXa+1)
+       else: #diagonal line segment
+           steepSlope = dYa > dXa
+           if steepSlope:
+               slope = dX/dY
+               if negY:
+                   itbuffer[:,1] = np.arange(P1Y-1,P1Y-dYa-1,-1)
+               else:
+                   itbuffer[:,1] = np.arange(P1Y+1,P1Y+dYa+1)
+               itbuffer[:,0] = (slope*(itbuffer[:,1]-P1Y)).astype(np.int) + P1X
+           else:
+               slope = dY/dX
+               if negX:
+                   itbuffer[:,0] = np.arange(P1X-1,P1X-dXa-1,-1)
+               else:
+                   itbuffer[:,0] = np.arange(P1X+1,P1X+dXa+1)
+               itbuffer[:,1] = (slope*(itbuffer[:,0]-P1X)).astype(np.int) + P1Y
+
+       #Remove points outside of image
+       colX = itbuffer[:,0]
+       colY = itbuffer[:,1]
+       itbuffer = itbuffer[(colX >= 0) & (colY >=0) & (colX<imageW) & (colY<imageH)]
+
+       #Get intensities from image_square ndarray
+       itbuffer[:,2] = image_square[itbuffer[:,1].astype(np.uint),itbuffer[:,0].astype(np.uint)]
+
+       return itbuffer
 
 if __name__ == '__main__':
     rospy.init_node('convert_to_topo_nav', anonymous=True)
