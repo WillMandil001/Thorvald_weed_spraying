20527bacc6b812a1571627d1e06ee7c0fb11b481
Paris(lab) <parischatz94@gmail.com>
2020-01-31 16:16:10 +0000

    Dynamic sprayer

--- CMP9767M/uol_cmp9767m_base/scripts/sprayer.py
+++ CMP9767M/uol_cmp9767m_base/scripts/sprayer.py
@@ -2,9 +2,8 @@
 
 import rospy
 from gazebo_msgs.srv import SpawnModel, SpawnModelRequest
-from std_srvs.srv import Empty
-from std_msgs.msg import String
 from uuid import uuid4
+from uol_cmp9767m_base.srv import y_axes_diff, y_axes_diffResponse
 
 BOX_SDF="""
 <?xml version='1.0'?>
@@ -47,109 +46,30 @@ BOX_SDF="""
 </sdf>
 """
 
-
 class Sprayer:
     # The request.initial_pose.position.y is the distance from the center of the sprayer
     def __init__(self):
         self.sdf = BOX_SDF
         #define the services
-        rospy.Service('spray1', Empty, self.spray1)
-        rospy.Service('spray2', Empty, self.spray2)
-        rospy.Service('spray3', Empty, self.spray3)
-        rospy.Service('spray4', Empty, self.spray4)
-        rospy.Service('spray5', Empty, self.spray5)
-        rospy.Service('spray6', Empty, self.spray6)
-        rospy.Service('spray7', Empty, self.spray7)
-
+        self.srv = rospy.Service('/thorvald_001/dynamic_sprayer', y_axes_diff, self.spray)
         #call the service to spawn a little rectangle on gazebo
         self.spawner = rospy.ServiceProxy(
             '/gazebo/spawn_sdf_model', SpawnModel)
 
-
-    def spray1(self, r):
-   
+    def spray(self, r):
         request = SpawnModelRequest()
         request.model_name = 'killbox_%s' % uuid4()
         request.model_xml = self.sdf
         request.reference_frame = 'thorvald_001/base_link'
         request.initial_pose.position.z = 0.005
         request.initial_pose.position.x = -0.45
-        request.initial_pose.position.y = 0.3
         request.initial_pose.orientation.w = 1.0
-        self.spawner(request)
-        return []
+        request.initial_pose.position.y = r.y_diff
 
-    def spray2(self, r):
-        request = SpawnModelRequest()
-        request.model_name = 'killbox_%s' % uuid4()
-        request.model_xml = self.sdf
-        request.reference_frame = 'thorvald_001/base_link'
-        request.initial_pose.position.z = 0.005
-        request.initial_pose.position.x = -0.45
-        request.initial_pose.position.y = 0.2
-        request.initial_pose.orientation.w = 1.0
-        self.spawner(request)
-        return []
 
-    def spray3(self, r):
-        request = SpawnModelRequest()
-        request.model_name = 'killbox_%s' % uuid4()
-        request.model_xml = self.sdf
-        request.reference_frame = 'thorvald_001/base_link'
-        request.initial_pose.position.z = 0.005
-        request.initial_pose.position.x = -0.45
-        request.initial_pose.position.y = 0.1
-        request.initial_pose.orientation.w = 1.0
+        # rospy.sleep(0.5)
         self.spawner(request)
-        return []
-
-    def spray4(self, r):
-        request = SpawnModelRequest()
-        request.model_name = 'killbox_%s' % uuid4()
-        request.model_xml = self.sdf
-        request.reference_frame = 'thorvald_001/base_link'
-        request.initial_pose.position.z = 0.005
-        request.initial_pose.position.x = -0.45
-        request.initial_pose.position.y = 0
-        request.initial_pose.orientation.w = 1.0
-        self.spawner(request)
-        return []
-
-    def spray5(self, r):
-        request = SpawnModelRequest()
-        request.model_name = 'killbox_%s' % uuid4()
-        request.model_xml = self.sdf
-        request.reference_frame = 'thorvald_001/base_link'
-        request.initial_pose.position.z = 0.005
-        request.initial_pose.position.x = -0.45
-        request.initial_pose.position.y = -0.1
-        request.initial_pose.orientation.w = 1.0
-        self.spawner(request)
-        return []
-
-    def spray6(self, r):
-        request = SpawnModelRequest()
-        request.model_name = 'killbox_%s' % uuid4()
-        request.model_xml = self.sdf
-        request.reference_frame = 'thorvald_001/base_link'
-        request.initial_pose.position.z = 0.005
-        request.initial_pose.position.x = -0.45
-        request.initial_pose.position.y = -0.2
-        request.initial_pose.orientation.w = 1.0
-        self.spawner(request)
-        return []
-
-    def spray7(self, r):
-        request = SpawnModelRequest()
-        request.model_name = 'killbox_%s' % uuid4()
-        request.model_xml = self.sdf
-        request.reference_frame = 'thorvald_001/base_link'
-        request.initial_pose.position.z = 0.005
-        request.initial_pose.position.x = -0.45
-        request.initial_pose.position.y = -0.3
-        request.initial_pose.orientation.w = 1.0
-        self.spawner(request)
-        return []
+        return True
 
 if __name__ == "__main__":
     rospy.init_node('sprayer')
