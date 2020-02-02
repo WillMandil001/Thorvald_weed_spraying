46f91ac471f0eaf43fb1ace9e557222332c5c025
bkbilly <bkbilly@hotmail.com>
2020-02-02 22:56:34 +0000

    publish real points

--- thorvald_sprayer_package/scripts/ros_spray.py
+++ thorvald_sprayer_package/scripts/ros_spray.py
@@ -6,6 +6,7 @@ import tf
 # Ros Messages
 from sensor_msgs.msg import PointCloud
 from uol_cmp9767m_base.srv import y_axes_diff, y_axes_diffRequest
+from geometry_msgs.msg import Point32
 
 
 class Sprayer():
@@ -13,6 +14,7 @@ class Sprayer():
     def __init__(self, robot):
         self.robot = robot
         self.sprayed = []  # Keep a list of sprayed points
+        self.real_sprayed = []
         self.spray_srv = rospy.ServiceProxy(
             "{}/dynamic_sprayer".format(self.robot),
             y_axes_diff)
@@ -49,9 +51,11 @@ class Sprayer():
             dx = abs(trans[0] - point.x)
             dy = trans[1] - point.y  # this creates mirror
             # Deside to spray
-            if dx < 0.1 and abs(dy) < 0.6:
+            if dx < 0.01 and abs(dy) < 0.5:
                 if point not in self.sprayed:
                     self.sprayed.append(point)
+                    real_point = Point32(trans[0], trans[1] - dy, point.z)
+                    self.real_sprayed.append(real_point)
                     print('spray!!!')
                     # Add delay
                     req = y_axes_diffRequest()
@@ -59,7 +63,7 @@ class Sprayer():
                     self.spray_srv(req)
 
         # Publish the Sprayed Points
-        self.sprayed_points_msg.points = self.sprayed
+        self.sprayed_points_msg.points = self.real_sprayed
         self.sprayed_points_msg.header.frame_id = 'map'
         self.sprayed_points_msg.header.stamp = time
         self.sprayed_points_pub.publish(self.sprayed_points_msg)    
