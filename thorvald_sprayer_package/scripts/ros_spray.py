4e67801c8b83a7338a81be7dadd656924a3c95c1
paris_from_lab <parischatz94@gmail.com>
2020-02-04 15:00:27 +0000

    small changes

--- thorvald_sprayer_package/scripts/ros_spray.py
+++ thorvald_sprayer_package/scripts/ros_spray.py
@@ -34,12 +34,11 @@ class Sprayer():
             PointCloud,
             queue_size=5)
 
-    def slep(self, current_distance):
+    def slep(self, sprayer_distance):
 
-        total_travel_time = 1
-        slep_time = current_distance/total_travel_time
+        total_travel_speed = 10
+        slep_time = sprayer_distance/total_travel_speed
         rospy.sleep(slep_time)
-        last_spray_pos = 0
 
     def spray_weed_callback(self, data):
         time = rospy.Time(0)
@@ -52,30 +51,52 @@ class Sprayer():
             # print(trans, rot)
         except Exception:
             return
+        
+        # First initialise current_y_sprayer = result from tf
+        current_y_sprayer = trans[1] # it's going to be updated according to the place it travels to.
 
+        print("bich lasagnia")
         # Iterate every detected Weed
-        for point in data.points:
-            # pos of robot
-            x_robot = trans[0] 
-            y_robot = trans[1]
-            # Difference in 'x' and 'y' frame
-            dx = abs(x_robot - point.x)
-            dy = y_robot - point.y  # this creates mirror effect
-            # Deside to spray
-            if dx < 0.01 and abs(dy) < 0.5:
-                if point not in self.sprayed:
-                    # calculate euclidean dist between robot and weed
-                    dist = math.sqrt(math.pow(x_robot - point.x,2)+ math.pow(y_robot - point.y,2))
-                    self.sprayed.append(point)  # add point in sprayed array
-                    # save the position of the sprayer (visualise in rviz)
-                    real_point = Point32(x_robot, y_robot - dist, point.z) 
-                    self.real_sprayed.append(real_point)
-                    # when sprayer moves sleep for travel time
-                    # self.slep(dy)
-                    print('I sprayied!!!', dy, dist)
-                    req = y_axes_diffRequest()
-                    req.y_diff = dy
-                    self.spray_srv(req)
+        # for point in data.points:
+        # pos of robot
+        x_sprayer = trans[0] 
+
+        # Difference in 'x' and 'y' frame
+        # dx = abs(x_sprayer - point.x)
+        # dy = current_y_sprayer - point.y  # this creates mirror effect
+
+        dx = abs(x_sprayer - (-2.5))
+        dy = current_y_sprayer - (-7.15)  # this creates mirror effect
+        
+        # Deside to spray
+        print("yeah")
+
+        if abs(dy) < 0.5:
+            #find distance between point and current sprayer position
+            sprayer_dist = current_y_sprayer - (-7.15) #point.y
+            print("I found weed with distance, ", sprayer_dist, current_y_sprayer)
+            # when sprayer moves sleep for travel time
+            self.slep(sprayer_dist)
+
+            # After moving (sleeping) update the current sprayer position to it's new position
+            current_y_sprayer = (-7.5) #point.y
+
+            if dx < 0.05 and abs(dy) < 0.5:
+                # if point not in self.sprayed:
+                # calculate euclidean dist between robot and weed
+                # dist = math.sqrt(math.pow(x_sprayer - point.x,2)+ math.pow(y_sprayer - point.y,2))
+
+                # self.sprayed.append(point)  # add point in sprayed array
+                # save the position of the sprayer (visualise in rviz)
+                #TODO fix mirroring
+                real_point = Point32(x_sprayer,  dy, 0)#point.z) 
+                self.real_sprayed.append(real_point)
+
+
+                req = y_axes_diffRequest()
+                req.y_diff = dy
+                self.spray_srv(req)
+                print('I sprayied!!!', dy)
 
         # Publish the Sprayed Points
         self.sprayed_points_msg.points = self.real_sprayed
