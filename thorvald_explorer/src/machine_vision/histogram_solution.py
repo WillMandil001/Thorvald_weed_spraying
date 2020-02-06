d89965249b64480767414f663c2098e66062c218
WillMandil001 <Will.Mandil@stelram.co.uk>
2020-02-06 12:56:13 +0000

    working histogram analysis method - still need to convert pixel coordinates to real world pose

--- thorvald_explorer/src/machine_vision/histogram_solution.py
+++ thorvald_explorer/src/machine_vision/histogram_solution.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python
 import tf
 import cv2
+import sys
 import math
-import tf2_ros
 import rospy
+import tf2_ros
+import operator
+import itertools
 import numpy as np
 import image_geometry
 import copy
 from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
+from scipy.misc import electrocardiogram
+from scipy.signal import find_peaks
+            
+
 
 from std_msgs.msg import String
 from sensor_msgs.msg import Image, CameraInfo
@@ -48,46 +56,16 @@ class convert_to_topo_nav():
 
         kernel = np.ones((3,3),np.uint8)
         opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-        return opening
+        kernel = np.ones((10,10),np.uint8)
+        dilate = cv2.dilate(opening, kernel, 1)
 
-    def calculate_crop_rows(self, cv_image):
-        # 1. divide image up into set of square images for histogram analysis on each.
-        imgx_l = cv_image.shape[1]
-        imgy_h = cv_image.shape[0]
-        box_size = 200
-        prev_point_l = 0
-        prev_point_h = 0
-        for i in range(0, imgx_l, box_size):
-            for j in range(0, imgy_h, box_size):
-                # print("i = ", i, "j = ", j)
-                image_square = cv_image[i:(i+box_size), j:(j+box_size)]
-                # 1. Find center of image.
-                imgx = (image_square.shape[1] / 2)
-                imgy = (image_square.shape[0] / 2)
+        return dilate
 
-                # 2. Calculate lines for each angle (0, 179) degrees and do for 
-                histogram = []
-                angles = []
-                for angle in range(0, 180, 10):
-                    histo = 0
-                    angle_rad = (angle * math.pi) / 180
-                    r = 100  # R = LENGTH OF LINE DRAWN. JUST NEEDS TO BE LONGER THAN THE EDGES OF THE image_square
-                    for a in range(-r, r + 1, max(1, int((r * 2)))):
-                      print a
-                        # xcirc = imgx + a * math.cos(angle_rad)
-                        # ycirc = imgy + a * math.sin(angle_rad)
-                        # points = self.createLineIterator((int(imgx), int(imgy)), (int(xcirc), int(ycirc)), image_square)
-                        # histo += sum(points[:, 2])
-
-
-                cv2.imshow("image_square", image_square)
-                k = cv2.waitKey(0)
-                if k ==27:
-                    break
-                cv2.destroyAllWindows()
-                # 3. buld histogram from sum of white pixels in line.
-
-    def perpendicular_cumulative_sections(self, cv_image):
+    def perpendicular_cumulative_sections(self, cv_image, color_image):
+        # cv2.imshow("cv_image", cv_image)
+        # k = cv2.waitKey(0)
+        # if k ==27:
+        #     pass
         imgx = (cv_image.shape[1] / 2)
         imgy = (cv_image.shape[0] / 2)
 
@@ -95,10 +73,12 @@ class convert_to_topo_nav():
         perpendicular_cumulative_section = []
         angles = []
         point = []
-        for angle in range(0, 180, 10):
+        max_angle = 180
+        step_size = 10
+        for angle in range(0, max_angle, step_size):
             cv_image_copy_1 = np.zeros([cv_image.shape[0], cv_image.shape[1]]) + angle + 1
             angle_rad = (angle * math.pi) / 180
-            r = 1000
+            r = min(imgx, imgy)
             for a in range(-r, r + 1, max(1, int((r * 2)))):
                 xcirc = imgx + a * math.cos(angle_rad)
                 ycirc = imgy + a * math.sin(angle_rad)
@@ -117,24 +97,121 @@ class convert_to_topo_nav():
                         cv_image_copy_2 = cv2.line(cv_image_copy_2, (int(indices[1][i]), int(indices[0][i])), (int(xcirc_1), int(ycirc_1)), 100, 1)
                         # cv_image_copy_2[indices[0][i], indices[1][i]] = 200
                     coordinates = np.where(cv_image_copy_2 == 100)
-                    sum = 0
+                    sum__ = 0
                     for coord in range(0, len(coordinates[0])):
                         if cv_image[coordinates[0][coord], coordinates[1][coord]] == 255:
                             # cv_image = cv2.circle(cv_image, (coordinates[1][coord], coordinates[0][coord]), 10, 100, 2)
                             # cumulative.append(coordinates[0][coord], coordinates[1][coord], angle)
-                            sum += 1
-                    cumulative_list.append(sum)
+                            sum__ += 1
+                    cumulative_list.append(sum__)
 
             print("angle: ", angle, "cumulative_list: ", cumulative_list)
             perpendicular_cumulative_section.append(cumulative_list)
-            # cv2.imshow("cv_image", cv_image)
+
+            # cv2.imshow("image_square", image_square)
             # k = cv2.waitKey(0)
-            # if k ==27:
+            # if k == 27:
             #     break
-        for i in range(0, len(perpendicular_cumulative_section), 2):
-            plt.plot(perpendicular_cumulative_section[i])
-            plt.ylabel(('angle: ' + str(i)))
-        plt.show()
+            # cv2.destroyAllWindows()
+        peaks_list = []
+        for i in range(0, len(perpendicular_cumulative_section)):
+            peaks, _ = find_peaks(perpendicular_cumulative_section[i], height=0)
+            sum_peaks_list = []
+            for j in peaks:
+                sum_peaks_list.append(perpendicular_cumulative_section[i][j])
+            peaks_list.append([peaks, sum_peaks_list])
+
+        mean_list = []
+        for line in peaks_list:
+            mean = sum(line[1]) / len(line[1])
+            mean_list.append(mean)
+
+        index, value = max(enumerate(mean_list), key=operator.itemgetter(1))
+        print index
+        # plt.plot(perpendicular_cumulative_section[index])
+        # plt.ylabel(('angle: ' + str(index)))
+        # plt.ylim([0, 700])
+        # plt.show()
+
+        # Now get the correct pixel points for the waypoints:
+        # 1. first get the angle correct:
+        # index -> angle:
+        angle = (index * step_size) - 90
+        angle_rad = (angle * math.pi) / 180
+
+        imgx = (cv_image.shape[1] / 2)
+        imgy = (cv_image.shape[0] / 2)
+
+        cv_image_copy_1 = np.zeros([cv_image.shape[0], cv_image.shape[1]])
+        r = min(imgx, imgy)
+        for a in range(-r, r + 1, max(1, int((r * 2)))):
+            xcirc = imgx + a * math.cos(angle_rad)
+            ycirc = imgy + a * math.sin(angle_rad)
+            cv_image_copy_1 = cv2.line(cv_image_copy_1, (int(imgx), int(imgy)), (int(xcirc), int(ycirc)), 150, 1)
+
+        indices = np.where(cv_image_copy_1 == 150)
+        n = 20  # How often to take perpendicular line segment
+        cumulative_list = []
+        for i in range(0, len(indices[0])):
+            if i % n == 1:
+                cv_image_copy_2 = np.zeros([cv_image.shape[0], cv_image.shape[1]])
+                for b in range(-r, r + 1, max(1, int((r * 2)))):
+                    xcirc_1 = indices[1][i] + b * math.cos(angle_rad - (math.pi / 2))
+                    ycirc_1 = indices[0][i] + b * math.sin(angle_rad - (math.pi / 2))
+                    cv_image_copy_2 = cv2.line(cv_image_copy_2, (int(indices[1][i]), int(indices[0][i])), (int(xcirc_1), int(ycirc_1)), 150, 1)
+                coordinates = np.where(cv_image_copy_2 == 150)
+                waypoint_graph = []
+                waypoint_graph_coords = []
+                for coord in range(0, len(coordinates[0])):
+                    if cv_image[coordinates[0][coord], coordinates[1][coord]] == 255:
+                        # cv_image = cv2.circle(cv_image, (coordinates[1][coord], coordinates[0][coord]), 10, 100, 2)
+                        # cumulative.append(coordinates[0][coord], coordinates[1][coord], angle)
+                        waypoint_graph_coords.append([1, coordinates[0][coord], coordinates[1][coord]])
+                        waypoint_graph.append(1)
+                    else:
+                        waypoint_graph_coords.append([0, coordinates[0][coord], coordinates[1][coord]])
+                        waypoint_graph.append(0)
+
+                peaks, _ = find_peaks(waypoint_graph, height=0)
+                if len(peaks) != 0:
+                    mean_peak_list = self.cluster_analysis(peaks, 20)
+
+                print("peaks: ", peaks)
+                plt.plot(waypoint_graph)
+                if len(peaks) != 0:
+                    for p in mean_peak_list:
+                        plt.plot(p, 1, "x")
+                plt.ylabel(('state_' + str(i)))
+                plt.ylim([0, 2])
+                # plt.savefig('WAYPOINTPLOTSMEAN_state_' + str(i))
+                plt.clf()
+
+                # now plot peaks on cv_image:::
+                if len(peaks) != 0:
+                    for pk in mean_peak_list:
+                        radius = 5
+                        color_image = cv2.circle(color_image, (waypoint_graph_coords[pk][2], waypoint_graph_coords[pk][1]), radius, (150, 20, 20), 3) 
+   
+        cv2.imshow("color_image", color_image)
+        k = cv2.waitKey(0)
+        if k == 27:
+            pass
+        cv2.destroyAllWindows()
+
+    def cluster_analysis(self, peaks, maxgap):
+        mean_peak_list = []
+        peaks.sort()
+        groups = [[peaks[0]]]
+        for x in peaks[1:]:
+            if abs(x - groups[-1][-1]) <= maxgap:
+                groups[-1].append(x)
+            else:
+                groups.append([x])
+
+        for p in groups:
+            mean_peak_list.append(sum(p) / len(p))
+
+        return mean_peak_list
 
 if __name__ == '__main__':
     rospy.init_node('convert_to_topo_nav', anonymous=True)
@@ -142,6 +219,4 @@ if __name__ == '__main__':
     raw_image = convert.import_image()
     no_soil_image = convert.remove_soil(raw_image)
     # convert.calculate_crop_rows(no_soil_image)
-    convert.perpendicular_cumulative_sections(no_soil_image)
-
-
+    convert.perpendicular_cumulative_sections(no_soil_image, raw_image)
